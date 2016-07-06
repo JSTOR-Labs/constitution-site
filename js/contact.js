a52e93a4928028eb926aac61e5da353009d30357
rsnyder <ron@snyderjr.com>
2016-07-06 16:57:47 -0400

    Various updates related to contact form.

---
+++ js/contact.js
@@ -0,0 +1,107 @@
+var captcha_widget;
+
+var loadCaptcha = function() {
+        captcha_widget = grecaptcha.render('recaptcha', {
+          'sitekey' : '6LeVuQ0TAAAAAPQ96Z4YC0lq-vJ78A088-C7OxvZ',
+          'callback' : function(response) {
+           console.log(response)
+        }
+        });
+      };
+
+
+ $(function() {
+
+function getCookie(name) {
+var cookieValue = null;
+if (document.cookie && document.cookie != '') {
+    var cookies = document.cookie.split(';');
+    for (var i = 0; i < cookies.length; i++) {
+        var cookie = jQuery.trim(cookies[i]);
+        // Does this cookie string begin with the name we want?
+        if (cookie.substring(0, name.length + 1) == (name + '=')) {
+            cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+            break;
+        }
+    }
+}
+    return cookieValue;
+}
+var csrftoken = getCookie('csrftoken');
+console.log(csrftoken);
+function csrfSafeMethod(method) {
+    // these HTTP methods do not require CSRF protection
+    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+}
+$.ajaxSetup({
+    beforeSend: function(xhr, settings) {
+        if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+            xhr.setRequestHeader("X-CSRFToken", csrftoken);
+        }
+    }
+});
+
+    $("#thankyou").css("display","block");
+    $("#thankyou").hide();
+    var dialog, form,
+    name = $( "#name" ),
+    email = $( "#email" ),
+    comments = $( "#comments" ),
+    allFields = $( [] ).add( name ).add( email ).add( comments );
+    
+    
+    function closeDialog() {
+        response = grecaptcha.getResponse(captcha_widget);
+        if(response){
+            $.post( "/labs-contact-us-captcha/", { name: $("#name").val(), email: $("#email").val(), subject: $("#subject").val(), comments: $("#comments").val(), captcharesponse: response})
+            .done(function(data) {
+                $('#thankyou').html('Thank you for your input!');
+                console.log("Message sent");
+            })
+            .fail(function(data) {
+                $('#thankyou').html('Something went wrong. Please try again or email <a href="mailto:labs@ithaka.org">labs@ithaka.org</a>');
+                console.log("Message failed");
+            })
+            .always(function(data) {
+                $("#thankyou").fadeIn().delay(4000).fadeOut();
+                dialog.dialog( "close" );
+            });
+        } else {
+            alert("Please enter the CAPTCHA or refresh the page");}
+    }      
+
+    dialog = $( "#contact-form" ).dialog({
+        autoOpen: false,
+        height: 650,
+        width: 500,
+        modal: true,
+        buttons: {
+    "Send Comments": closeDialog,
+    Cancel: function() {
+        dialog.dialog( "close" );
+    }
+    },
+    close: function() {
+    form[ 0 ].reset();
+    }
+    });
+    form = dialog.find( "form" ).on( "submit", function( event ) {
+    });
+    
+    $( "#showContact" ).on( "click", function() {
+        dialog.dialog( "open" );
+    });
+
+     function getUrlVars() {
+         var vars = [], hash;
+         var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+         for (var i = 0; i < hashes.length; i++) {
+             hash = hashes[i].split('=');
+             vars.push(hash[0]);
+             vars[hash[0]] = hash[1];
+         }
+         return vars;
+     }
+
+
+ });
--- support.md
+++ support.md
@@ -4,6 +4,8 @@ title: Support
 description: For <i>Understanding the U.S. Constitution</i>
 ---
 
+<a id="showContact" class="button">Send Comments</a>
+
 ### [Contact Us](http://labs.jstor.org/labs-contact-us-captcha/?subject=Understanding the US Constitution&next=/constitution/support.html)
 
 We want to hear from you how to make this app better!  [**Send us your questions, comments and suggestions.**​](http://labs.jstor.org/labs-contact-us-captcha/?subject=Understanding the US Constitution&next=/constitution/support.html)
@@ -15,3 +17,18 @@ We want to hear from you how to make this app better!  [**Send us your questions
 * It is possible to select a clause (such as the signatures in Article 7) with 0 articles quoting or referring to it, leading to a zero-results page.
 * Occasionally the icon to refine your search results appears at the bottom of the screen (when this happens, restart the app to correct the display.)
 
+    <div id="thankyou">Thank you for your input!</div>
+    <div id="contact-form" title="Send Comments" >
+        <form method="post" action="/labs-contact-us/">
+            <label for="name">Name</label>
+            <input type="text" name="name" id="name" value="" class="text ui-widget-content ui-corner-all">
+            <label for="email">Email</label>
+            <input type="text" name="email" id="email" value="" class="text ui-widget-content ui-corner-all">
+            <input type="hidden" name="subject" id="subject" value="Understanding the US Constitution">
+            <label for="comments">Comments and Suggestions</label>
+            <textarea name="comments" id="comments" rows="6" cols="70" value="" class="text ui-widget-content ui-corner-all"></textarea>
+            <div id="recaptcha"></div>
+            <input type="submit" tabindex="-1" style="position:absolute; top:-1000px" >
+        </form>
+        <script src="https://www.google.com/recaptcha/api.js?onload=loadCaptcha&render=explicit"></script>
+    </div>
